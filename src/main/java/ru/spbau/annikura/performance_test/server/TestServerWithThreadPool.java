4b3360329e72965a44719a02a9620783b3dbe9c4
Ann <annikura@yandex.ru>
2018-06-19 22:30:28 +0300

    Added server with thread pool

--- src/main/java/ru/spbau/annikura/performance_test/server/TestServerWithThreadPool.java
+++ src/main/java/ru/spbau/annikura/performance_test/server/TestServerWithThreadPool.java
@@ -1,7 +1,32 @@
 package ru.spbau.annikura.performance_test.server;
 
+import java.io.IOException;
+import java.nio.channels.ServerSocketChannel;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.logging.Logger;
+
 public class TestServerWithThreadPool implements PerformanceTestServerInterface {
     public void start(int port) {
-
+        Logger.getAnonymousLogger().info("Starting thread pool server");
+        ServerSocketChannel serverSocketChannel;
+        try {
+            serverSocketChannel = ServerSocketChannel.open();
+        } catch (IOException e) {
+            Logger.getAnonymousLogger().severe("Unable to run server socket. Shutting down...: " + e.getMessage());
+            return;
+        }
+        ExecutorService threadPool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        while (true) {
+            try {
+                Runnable runnable = new TestServerWithThreadPoolRunnable(
+                        serverSocketChannel.accept(),
+                        threadPool, Executors.newSingleThreadExecutor());
+                new Thread(runnable).start();
+                Logger.getAnonymousLogger().info("Accepted new connection");
+            } catch (IOException e) {
+                Logger.getAnonymousLogger().warning("Failed to establish connection with client: " + e.getMessage());
+            }
+        }
     }
 }
--- src/main/java/ru/spbau/annikura/performance_test/server/TestServerWithThreadPoolRunnable.java
+++ src/main/java/ru/spbau/annikura/performance_test/server/TestServerWithThreadPoolRunnable.java
@@ -1,4 +1,54 @@
 package ru.spbau.annikura.performance_test.server;
 
-public class TestServerWithThreadPoolRunnable {
+import org.jetbrains.annotations.NotNull;
+import ru.spbau.annikura.performance_test.server.tasks.ReadingRequestTask;
+import ru.spbau.annikura.performance_test.server.tasks.SortingTask;
+import ru.spbau.annikura.performance_test.server.tasks.TaskContext;
+import ru.spbau.annikura.performance_test.server.tasks.WritingResponseTask;
+import sun.rmi.runtime.Log;
+
+import javax.xml.ws.Holder;
+import java.nio.channels.SocketChannel;
+import java.util.concurrent.ExecutorService;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.logging.Logger;
+
+public class TestServerWithThreadPoolRunnable implements Runnable {
+    private SocketChannel channel;
+    private final ExecutorService threadPool;
+    private final ExecutorService outputService;
+
+    public TestServerWithThreadPoolRunnable(@NotNull final SocketChannel channel,
+                                            @NotNull final ExecutorService pool,
+                                            @NotNull final ExecutorService outputService) {
+        this.channel = channel;
+        this.threadPool = pool;
+        this.outputService = outputService;
+    }
+
+    @Override
+    public void run() {
+        Holder<Boolean> isLast = new Holder<>(false);
+        while (!isLast.value) {
+            TaskContext context = new TaskContext();
+            new ReadingRequestTask().call(context.new ReadingTaskContext(channel), sortingTaskContext -> {
+                threadPool.submit(() -> {
+                    new SortingTask().call(sortingTaskContext, writingTaskContext -> {
+                        outputService.submit(() -> {
+                            new WritingResponseTask().call(writingTaskContext, taskContext -> {
+                                isLast.value = taskContext.isLast();
+                            }, (writingTaskContext1, e) -> {
+                                Logger.getAnonymousLogger().severe("Writing failed: " + e.getMessage());
+                            });
+                        });
+                    }, (sortingTaskContext1, e) -> {
+                        Logger.getAnonymousLogger().severe("Sort failed: " + e.getMessage());
+                    });
+                });
+            }, (readingTaskContext, e) -> {
+                Logger.getAnonymousLogger().severe("Reading failed: " + e.getMessage());
+            });
+        }
+    }
 }
